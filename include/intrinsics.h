179d06a54cacc9268116cfcb54ca9455daa73cc6
Tom Tzook <tomtzook@gmail.com>
2021-12-25 21:30:26 +0200

    some intrinsics

--- include/intrinsics.h
+++ include/intrinsics.h
@@ -2,8 +2,9 @@
 
 #include "types.h"
 
-
+int memcmp(const void* s1, const void* s2, size_t size);
 void memset(void* dest, uint8_t value, size_t size);
 void* memcpy(void* dest, const void* src, size_t size);
 
 size_t strlen(const char* s);
+int strcmp(const char* s1, const char* s2);
--- src/intrinsics.cpp
+++ src/intrinsics.cpp
@@ -1,6 +1,16 @@
 #include "intrinsics.h"
 
 
+int memcmp(const void* s1, const void* s2, size_t size) {
+    auto* ptr_s1 = (uint8_t*) s1;
+    auto* ptr_s2 = (uint8_t*) s2;
+    while (size-- > 0) {
+        if (*ptr_s1++ != *ptr_s2++)
+            return ptr_s1[-1] < ptr_s2[-1] ? -1 : 1;
+    }
+    return 0;
+}
+
 void memset(void* dest, uint8_t value, size_t size) {
     auto* ptr = (uint8_t*) dest;
     while ((size--)) {
@@ -29,4 +39,15 @@ size_t strlen(const char* s) {
     }
 
     return count;
+}
+
+int strcmp(const char* s1, const char* s2) {
+    while (*s1) {
+        if (*s1 != *s2) {
+            break;
+        }
+        ++(s1);
+        ++(s2);
+    }
+    return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
