9d8f4b8aef811c3e14bacd7ebf69a05de2d51c92
Tom Tzook <tomtzook@gmail.com>
2021-12-25 20:51:59 +0200

    hack for disabling default but allow partial

--- include/x86/common.h
+++ include/x86/common.h
@@ -8,10 +8,24 @@
 
 namespace x86 {
 
-template<typename _t>
+template<typename T>
+struct __hack : public meta::false_type {};
+
+
+template<
+        typename _t,
+        typename meta::enable_if<
+                __hack<_t>::value,
+                bool>::type = 0
+>
 _t read() noexcept = delete;
 
-template<typename _t>
+template<
+        typename _t,
+        typename meta::enable_if<
+                __hack<_t>::value,
+                bool>::type = 0
+>
 void write(const _t& t) noexcept = delete;
 
 }
