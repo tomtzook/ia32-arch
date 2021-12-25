7bff1122b6f947de8ce5f7879a2ef28acc26f9ca
Tom Tzook <tomtzook@gmail.com>
2021-12-25 21:06:31 +0200

    more types

--- include/types.h
+++ include/types.h
@@ -10,6 +10,7 @@ typedef signed int int32_t;
 typedef signed long int64_t;
 typedef int64_t intn_t;
 typedef int64_t intptr_t;
+typedef long int intmax_t;
 
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
@@ -17,6 +18,7 @@ typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
 typedef uint64_t uintn_t;
 typedef uint64_t uintptr_t;
+typedef unsigned long int uintmax_t;
 #else
 typedef signed char int8_t;
 typedef signed short int16_t;
@@ -24,6 +26,7 @@ typedef signed long int32_t;
 typedef signed long long int64_t;
 typedef int32_t intn_t;
 typedef int32_t intptr_t;
+typedef long long int intmax_t;
 
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
@@ -31,6 +34,7 @@ typedef unsigned long uint32_t;
 typedef unsigned long long uint64_t;
 typedef uint32_t uintn_t;
 typedef uint32_t uintptr_t;
+typedef unsigned long long int uintmax_t;
 #endif
 
 typedef unsigned long int size_t;
